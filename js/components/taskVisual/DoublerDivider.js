0a96ba2b912418e29d66bc035e651bb42be3649c
iamgo100 <gufkz.tkrbyf@yandex.ru>
2023-07-09 22:24:34 +0300

    hotfix: fix src

--- js/components/taskVisual/DoublerDivider.js
+++ js/components/taskVisual/DoublerDivider.js
@@ -4,7 +4,7 @@ export default {
     <div class="left-content colomn-cont">
         <div class="target-dd inline-cont">
             <span class="value">{{start}}</span>
-            <img src="/assets/next.png" class="arrow">
+            <img src="assets/next.png" class="arrow">
             <span class="value">{{end}}</span>
         </div>
         <div class="current-dd colomn-cont">
